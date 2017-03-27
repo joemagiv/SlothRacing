8cef640a5faa74fb0bc4b4cf7be12f52a12eb706
joemag <joemagiv@gmail.com>
2017-03-26 21:41:57 -0700

    Updates, hooked up scene with stats. Need to work on race simulations.

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -12,6 +12,8 @@ public class GameController : MonoBehaviour {
     public int slothAgility;
     public int slothGrit;
 
+    public Text availableStatPointsText;
+
     public Text slothSpeedText;
     public Text slothStaminaText;
     public Text slothAgilityText;
@@ -19,9 +21,98 @@ public class GameController : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-		
+        updateText();
 	}
-	
+
+    public void increaseSpeed()
+    {
+        if (availableStatPoints > 0)
+        {
+            slothSpeed++;
+            availableStatPoints--;
+        }
+        updateText();
+    }
+
+    public void decreaseSpeed()
+    {
+        if (slothSpeed > 0)
+        {
+            slothSpeed--;
+            availableStatPoints++;
+        }
+        updateText();
+    }
+
+    public void increaseStamina()
+    {
+        if (availableStatPoints > 0)
+        {
+            slothStamina++;
+            availableStatPoints--;
+        }
+        updateText();
+    }
+
+    public void decreaseStamina()
+    {
+        if (slothStamina > 0)
+        {
+            slothStamina--;
+            availableStatPoints++;
+        }
+        updateText();
+    }
+
+    public void increaseAgility()
+    {
+        if (availableStatPoints > 0)
+        {
+            slothAgility++;
+            availableStatPoints--;
+        }
+        updateText();
+    }
+
+    public void decreaseAgility()
+    {
+        if (slothAgility > 0)
+        {
+            slothAgility--;
+            availableStatPoints++;
+        }
+        updateText();
+    }
+
+    public void increaseGrit()
+    {
+        if (availableStatPoints > 0)
+        {
+            slothGrit++;
+            availableStatPoints--;
+        }
+        updateText();
+    }
+
+    public void decreaseGrit()
+    {
+        if (slothGrit > 0)
+        {
+            slothGrit--;
+            availableStatPoints++;
+        }
+        updateText();
+    }
+
+    private void updateText()
+    {
+        availableStatPointsText.text = "Available Points: " + availableStatPoints.ToString();
+        slothSpeedText.text = "Speed: " + slothSpeed.ToString();
+        slothStaminaText.text = "Stamina: " + slothStamina.ToString();
+        slothAgilityText.text = "Agility: " + slothAgility.ToString();
+        slothGritText.text = "Grit: " + slothGrit.ToString();
+    }
+
 	// Update is called once per frame
 	void Update () {
 		
